84e1152ee54b4df3bdb1d3db0104f3680e04cc77
이현규 <hklee@wemakeprice.com>
2020-08-18 11:38:50 +0900

    feat(common): fix Model PropertyName

--- RxTestSample/Model/SearchModel.swift
+++ RxTestSample/Model/SearchModel.swift
@@ -9,38 +9,38 @@
 import Foundation
 
 // MARK: - SearchModel
-struct SearchModel {
+struct SearchModel: Codable {
     let totalCount: Int?
     let incompleteResults: Bool?
     let items: [SearchItem]?
 }
 
 // MARK: - Item
-struct SearchItem {
+struct SearchItem: Codable {
     let id: Int?
-    let nodeID, name, fullName: String?
+    let node_id, name, full_name: String?
     let owner: Owner?
     let itemPrivate: Bool?
-    let htmlURL: String?
-    let itemDescription: String?
+    let html_url: String?
+    let description: String?
     let fork: Bool?
     let url: String?
-    let createdAt, updatedAt, pushedAt: Date?
+    let created_at, updated_at, pushed_at: Date?
     let homepage: String?
-    let size, stargazersCount, watchersCount: Int?
+    let size, stargazers_count, watchers_count: Int?
     let language: String?
-    let forksCount, openIssuesCount: Int?
-    let masterBranch, defaultBranch: String?
+    let forksCount, open_issues_count: Int?
+    let master_branch, default_branch: String?
     let score: Int?
 }
 
 // MARK: - Owner
-struct Owner {
+struct Owner: Codable {
     let login: String?
     let id: Int?
-    let nodeID: String?
-    let avatarURL: String?
-    let gravatarID: String?
-    let url, receivedEventsURL: String?
+    let node_id: String?
+    let avatar_url: String?
+    let gravatar_id: String?
+    let url, received_events_url: String?
     let type: String?
 }
